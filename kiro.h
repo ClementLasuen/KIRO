3d2559f0d3f4445e2a776bef5814c8412f8494db
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-11-29 15:11:06 +0100

    anglais

--- kiro.h
+++ kiro.h
@@ -3,15 +3,13 @@
 
 
 
-class sommet{
+class node{
     double x,y;
-    bool type_sommet; // 0 si distribution, 1 si antenne
+    bool node_type; // 0 si distribution, 1 si antenne
     int indice;
 public :
     void set(double x1, double y1) {x=x1; y=y1;}
     double get_x() {return x;}
     double get_y() {return y;}
-    sommet (double x1, double y1, bool t, int i) { x=x1; y=y1; type_sommet =t; indice =i;}
-
-
+    node (double x1, double y1, bool t, int i) { x=x1; y=y1; type_sommet =t; indice =i;}
 };
