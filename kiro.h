44623311bab445d76d74b6873a06b566d5212e16
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-11-29 15:23:56 +0100

    ajout classe arc

--- kiro.h
+++ kiro.h
@@ -12,6 +12,7 @@ public :
     double get_x() {return x;}
     double get_y() {return y;}
     node (double x1, double y1, bool t, int i) { x=x1; y=y1; node_type =t; indice =i;}
+    node () {}
 };
 
 class arc{
