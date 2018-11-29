18ce38daf53f94f59b680cb65c77e989a0c9e957
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-11-29 16:12:30 +0100

    nouvelles fonctions

--- kiro.h
+++ kiro.h
@@ -1,5 +1,7 @@
 #pragma once
 
+
+#include <time.h>
 #include <math.h>
 #include <vector>
 using namespace std;
@@ -15,6 +17,8 @@ public :
     node (double x1, double y1, bool t, int i) { x=x1; y=y1; node_type =t; indice =i;}
     node () {}
     int get_indice() {return indice;}
+    bool is_in(vector<node> V);
+    bool is_terminal() {return node_type;}
 };
 
 class arc{
@@ -27,3 +31,7 @@ public :
 };
 
 double cost(vector<node> C, vector<double> lenght);
+
+
+// Assume that the first element is the distribution
+void change_one_node(vector<node> circuit, vector<node> nodes, vector<double> lenght);
