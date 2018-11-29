67df9f6715367d53966ac4c5b4886246c5077634
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-11-29 14:55:43 +0100

    ajout d ela classe sommet

---
+++ kiro.h
@@ -0,0 +1,17 @@
+#pragma once
+
+
+
+
+class sommet{
+    double x,y;
+    bool type_sommet; // 0 si distribution, 1 si antenne
+
+public :
+    void set(double x1, double y1) {x=x1; y=y1;}
+    double get_x() {return x;}
+    double get_y() {return y;}
+    sommet (double x1, double x2, bool t) { x=x1; y=y1; type_sommet =t;}
+
+
+};
