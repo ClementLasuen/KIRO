7212457e9d2917f062aec70e7f8c1eabdfbbcc34
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-11-29 14:58:38 +0100

    modif classe sommet

--- kiro.h
+++ kiro.h
@@ -6,12 +6,12 @@
 class sommet{
     double x,y;
     bool type_sommet; // 0 si distribution, 1 si antenne
-
+    int indice;
 public :
     void set(double x1, double y1) {x=x1; y=y1;}
     double get_x() {return x;}
     double get_y() {return y;}
-    sommet (double x1, double x2, bool t) { x=x1; y=y1; type_sommet =t;}
+    sommet (double x1, double x2, bool t, int i) { x=x1; y=y1; type_sommet =t; indice =i;}
 
 
 };
