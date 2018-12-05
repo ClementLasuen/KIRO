e853c72a080d1adb2f38c848cc5735d06477afd1
Quentin Spinat <qspinat@gmail.com>
2018-12-05 10:18:45 +0100

    rien

--- main.cpp
+++ main.cpp
@@ -12,23 +12,24 @@ int main()
 
     vector<vector<node>> data = clustering(distribution, terminal, D);
 
-    int N = 10000;
+    int N = 1000000;
     int cnt = 0;
 
     //switch_chain(data,D);
 
 
     int c = cost_solution( data, D);
+    cout << c << endl;
     int c_test;
     while(cnt<N){
         vector<vector<node>> data_test = data;
         echange_aleat(data_test,distribution, terminal);
         c_test = cost_solution(data_test, D);
-        cout << c_test << endl;
         if (c_test< c) {
             data = data_test;
             c = c_test;
         }
+        if (cnt%(N/100)==0) cout<<cnt/(N/100)<<endl;
         cnt+=1;
     }
 
