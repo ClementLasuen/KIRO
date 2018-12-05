6f02f0838fba029c964a6e506d95e31a654e4bbf
Clement LASUEN <clement.lasuen@eleves.enpc.fr>
2018-12-05 09:06:45 +0100

    cost3

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ int main()
 
     int n = read_file(string("pim/distances.csv"),string("pim/nodes0.csv"), D, distribution, terminal);
 
-    vector<vector<node>> data = clustering(distribution, terminal, D);
+    vector<vector<node> > data = clustering(distribution, terminal, D);
 
     int N = 10000;
     int cnt = 0;
@@ -21,7 +21,7 @@ int main()
     int c = cost_solution( data, D);
     int c_test;
     while(cnt<N){
-        vector<vector<node>> data_test = data;
+        vector<vector<node> > data_test = data;
         echange_aleat(data_test,distribution, terminal);
         c_test = cost_solution(data_test, D);
         cout << c_test << endl;
