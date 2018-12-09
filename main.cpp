cd1c213e95f4283bb0d6d8317db54a87d7493f23
Hugo <nayw87@gmail.com>
2018-12-09 17:37:11 +0100

    nouvelle version de l'heuristique

--- main.cpp
+++ main.cpp
@@ -52,49 +52,13 @@ int main()
 
 
     int n_clustering = distribution.size() + terminal.size();
-    vector<int> index_clusters;
-    for (int i=0; i<terminal.size(); i++) {
-        int index = 0;
-        int min_distance = D[n*terminal[i].get_indice() + distribution[0].get_indice()];
-        for (int j=0; j<distribution.size(); j++) {
-            if (D[n*terminal[i].get_indice() + distribution[j].get_indice()] < min_distance) {
-                min_distance = D[n*terminal[i].get_indice() + distribution[j].get_indice()];
-                index = j;
-            }
-        }
-        index_clusters.push_back(index);
-    }
-    vector<vector<node>> data_clustering(distribution.size());
-    for (int i=0; i<distribution.size(); i++) {
-        data_clustering[i].push_back(distribution[i]);
-    }
-    for (int i=0; i<terminal.size(); i++) {
-        data_clustering[index_clusters[i]].push_back(terminal[i]);
-    }
-    for (int i=0; i<data_clustering.size(); i++) {
-        // On ordonne data pour le ieme distributeur de facon a avoir le (j+1)eme element qui est le proche voisin
-        // du jeme element parmi ceux qu'on a pas encore parcouru pour tout j
-        for (int j=0; j<data_clustering[i].size()-1; j++) {
-            int distance_min = D[n*data_clustering[i][j].get_indice() + data_clustering[i][j+1].get_indice()];
-            int index = j+1;
-            for (int k=j+2; k<data_clustering[i].size(); k++) {
-                if (D[n*data_clustering[i][j].get_indice() + data_clustering[i][k].get_indice()] < distance_min) {
-                    distance_min = D[n*data_clustering[i][j].get_indice() + data_clustering[i][k].get_indice()];
-                    index = k;
-                }
-            }
-            data_clustering[i].insert(data_clustering[i].begin()+j+1, data_clustering[i][index]);
-            data_clustering[i].erase(data_clustering[i].begin()+index+1);
-        }
-    }
-
-    int rep = 10000;
-    int best_cost = cost_solution(data, D);
-    cout << best_cost << endl;
+    int rep = 1000;
     vector<vector<vector<node>>> best_data = clustering(distribution, terminal, D);
-    for (int indice=0; indice<data_clustering.size(); indice++) {
+    int best_cost = cost_solution(best_data, D);
+    cout << best_cost << endl;
+    for (int indice=0; indice<best_data.size(); indice++) {
         for (int i=0; i<rep; i++) {
-            vector<vector<vector<node>>> new_data = heuristic_loop(data_clustering, n_clustering, D, indice);
+            vector<vector<vector<node>>> new_data = heuristic_loop(best_data, n_clustering, D, indice);
             int new_cost = cost_solution(new_data, D);
             if (new_cost < best_cost) {
                 best_cost = new_cost;
@@ -102,16 +66,6 @@ int main()
                 cout << best_cost << endl;
             }
         }
-        vector<node> best_loop_chain;
-        for (int j=0; j<best_data[indice][0].size(); j++) {
-            best_loop_chain.push_back(best_data[indice][0][j]);
-        }
-        for (int k=1; k<best_data[indice].size(); k++) {
-            for (int j=1; j<best_data[indice][k].size(); j++) {
-                best_loop_chain.push_back(best_data[indice][k][j]);
-            }
-        }
-        data_clustering[indice] = best_loop_chain;
         cout << "indice : " << indice << endl;
         write(best_data, string("resu_pim.txt"));
     }
